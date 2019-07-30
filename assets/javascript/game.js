20728e81419c51b108504a64df5552a70474a85a
LahRee <Laurie.Strecker@gmail.com>
2019-07-30 16:34:23 -0700

    crystal buttons are console.log random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,13 +9,14 @@ var win;
 //Random Number Generator
 random_result = Math.floor(Math.random() * 69 + 30);  //hoisting
 
+//New Random number generates everytime we Win or Lose
 $("#result").html('Random Result: '+ random_result);
 
 //A game with 4 crystals photos as buttions
 //Every crystal needs a random number between 1-12
 for (var i = 0; i < 4; i++) {
 
-  var random = Math.floor(Math.random() * 12)
+  var random = Math.floor(Math.random() * 11) + 1;
   console.log(random);
 
   var crystal = $("<div>");
@@ -24,14 +25,17 @@ for (var i = 0; i < 4; i++) {
         "class":'crystal',
         "data-random": random
       });
-
   
   $(".crystals").append(crystal);
     }
-//New Random number generates everytime we Win or Lose
 
 //when clicking crystal it should add numbers up 
 //untill it meets or exceeds target number
+$(".crystal").on('click', function () {
+
+  console.log($(this).attr('data-random'));
+   
+});
 
 //If the total is = to target#, then increment win counter
 
