43677990ee0e9a145b05fa6acde3c407325ae2cb
LahRee <Laurie.Strecker@gmail.com>
2019-07-30 12:40:07 -0700

    pseudocode, fixed file connections

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//PSEUDOCODE
+
+//global variables
+var randomNumber;
+var lost;
+var win;
+
+
+//A game with 4 crystals photos as buttions
+//Every crystal needs a random number between 1-12
+
+//New Random number generates everytime we Win or Lose
+
+//when clicking crystal it should add numbers up 
+//untill it meets or exceeds target number
+
+//If the total is = to target#, then increment win counter
+
+//if total is > than target# then we start over, & decrement win counter
